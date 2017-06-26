7e7efc3a9ec9b658748700158d82340fbedce641
Sh4wNy <markusme@edu.aau.at>
2017-06-27 00:14:32 +0200

    updated solver

--- src/solver.cpp
+++ src/solver.cpp
@@ -5,125 +5,176 @@
 
 #include "ourParser.h"
 
-
 class Solver: public Gecode::IntMinimizeSpace
 {
 public:
 
-    std::map<std::string, int> predicatesMap; // stores predicates as key -> values inside a map
-    std::map<std::string, int> actionsMap; // stores actions as key -> values inside a map
+    bool showOutput = false;
 
-    std::vector<Gecode::BoolVarArray> predicatesHistory_;
-    std::vector<Gecode::BoolVarArgs> actionHistory_;
+    std::map<std::string, int> predicatesMap;   // stores predicates as key -> values inside a map
+    std::map<std::string, int> actionsMap;      // stores actions as key -> values inside a map
+
+    std::vector<Gecode::BoolVarArray> predicatesHistory;
+    std::vector<Gecode::BoolVarArray> actionHistory;
 
     /**
-     * reads all neccessary data from the domain and stores it
-     * in appropriate data-structures for gecode
+     * reads all necessary data from the domain and stores it
+     * in appropriate data-structures to use with gecode
      * @param domain the domain, from which the data will be read
      */
     void readDataFromDomain(int horizon, VAL::domain *domain) {
         int i = 0; // default loop-variable
 
+        if(showOutput)
+            cout << "found predicates: \n";
         // get all predicates from the domain and save them into our predicatesMap
-        for(list<VAL::pred_decl*>::iterator predicate = domain->predicates->begin(); predicate != domain->predicates->end(); predicate++)
-        {
+        for(list<VAL::pred_decl*>::iterator predicate = domain->predicates->begin(); predicate != domain->predicates->end(); predicate++) {
             predicatesMap.insert(std::pair<std::string, const int>((*predicate)->getPred()->getName(), i));
             i++;
-            std::cout << i << ": " << (*predicate)->getPred()->getName() << "\n";
+            if(showOutput)
+                std::cout << i << ": " << (*predicate)->getPred()->getName() << "\n";
         }
 
         i = 0;
 
+        if(showOutput)
+            cout << "\nfound actions: \n";
         // get all actions from the domain and save them into our actionsMap
         for(list<VAL::operator_*>::iterator action = domain->ops->begin(); action != domain->ops->end(); action++)
         {
             actionsMap.insert(std::pair<std::string, const int>((*action)->name->getName(), i));
             i++;
-            std::cout << i << ": " << (*action)->name->getName() << "\n";
+            if(showOutput)
+                std::cout << i << ": " << (*action)->name->getName() << "\n";
         }
 
-        //construct a variable for each feature at each time step from 0 to horizon
+        //construct |horizon| BoolVarArrays for our predicatesHistory
         for( int i = 0; i < horizon; i++ ) {
-            //add an array with all features for each step in the timeline
-            predicatesHistory_.push_back(Gecode::BoolVarArray(*this, domain->predicates->size(), 0, 1));
+            // constructor:
+            // Gecode::BoolVarArray(Space & home, int n, int min, int max)
+
+            predicatesHistory.push_back(Gecode::BoolVarArray(*this, domain->predicates->size(), 0, 1));
         }
 
-        //construct a boolean variable for each action at each time step from 0 to horizon - 1
+        //construct |horizon| BoolVarArrays for our actionHistory
         for( int i = 0; i < horizon - 1; i++ ) {
-            actionHistory_.push_back(Gecode::BoolVarArray(*this, (int)domain->ops->size(), 0, 1));
+            // constructor:
+            // Gecode::BoolVarArray(Space & home, int n, int min, int max)
+            actionHistory.push_back(Gecode::BoolVarArray(*this, (int)domain->ops->size(), 0, 1));
         }
     }
 
+    /**
+     * parses a precondition
+     * @param goal
+     * @param horizon
+     * @param success
+     * @return
+     */
+    Gecode::BoolVar parsePreconditions(const VAL::goal* goal, int horizon) {
+        // check if goal is:
+        //      - simple
+        //      - a conjunction
+        //      - a negation
+        //      - a disjunction
 
-    Gecode::BoolVar parsePreconditions(const VAL::goal* goal, int k, bool* success) {
-        const VAL::simple_goal *simple = dynamic_cast<const VAL::simple_goal *>(goal);
-        const VAL::conj_goal *conj = dynamic_cast<const VAL::conj_goal *>(goal);
-        const VAL::neg_goal *neg = dynamic_cast<const VAL::neg_goal *>(goal);
-        const VAL::disj_goal *disj = dynamic_cast<const VAL::disj_goal *>(goal);
+        const VAL::simple_goal *simpleGoal = dynamic_cast<const VAL::simple_goal *>(goal);
+        const VAL::conj_goal *conjunctionGoal = dynamic_cast<const VAL::conj_goal *>(goal);
+        const VAL::neg_goal *negationGoal = dynamic_cast<const VAL::neg_goal *>(goal);
+        const VAL::disj_goal *disjunctionGoal = dynamic_cast<const VAL::disj_goal *>(goal);
 
-        if (simple) {
-            int id = predicatesMap.find(simple->getProp()->head->getName())->second;
-            cout << "simple " << std::endl;
-            return predicatesHistory_.at(k)[id];
-        } else if (conj) {
+        // if the goal is a simple one
+        if(simpleGoal) {
+            if(showOutput)
+                cout << *simpleGoal << "\n --- \n";
 
-            const VAL::goal_list *goals;
-            goals = conj->getGoals();
-            Gecode::BoolVar *tmp;
+            // get the index of the predicate in our predicateMap
+            int id = predicatesMap.find(simpleGoal->getProp()->head->getName())->second;
 
-            int size = conj->getGoals()->size();
+            // return the history-entry for this predicate
+            return predicatesHistory.at(horizon)[id];
+        }
 
-            if (size < 2) {
-                *success = false;
-                return Gecode::BoolVar();
-            }
+        // if the goal is a negation
+        if(negationGoal) {
+            if(showOutput)
+                cout << *negationGoal << "\n --- \n";
+            // return the expression for the negation of the precondition
+            return Gecode::expr(*this, not(parsePreconditions(negationGoal->getGoal(), horizon)));
+        }
 
-            tmp = new Gecode::BoolVar[size];
+        // if the goal is a conjunction
+        if(conjunctionGoal) {
+            if(showOutput)
+                cout << *conjunctionGoal << "\n --- \n";
+
+            // get all the goals
+            const VAL::goal_list *goals = conjunctionGoal->getGoals();
+
+            // number of goals
+            int goalCount = conjunctionGoal->getGoals()->size();
+
+            // temporary BoolVar-array for nested subgoals
+            Gecode::BoolVar *temp = new Gecode::BoolVar[goalCount];
+
             int i = 0;
-            for (auto precon = goals->begin(); precon != goals->end(); precon++) {
-                tmp[i] = parsePreconditions(*precon, k, success);
+
+            // for each element of the conjunction, parse it again to see, if there are nested goals in it
+            for (list<VAL::goal*>::const_iterator precondition = goals->begin(); precondition != goals->end(); precondition++) {
+                temp[i] = parsePreconditions(*precondition, horizon);
                 i++;
             }
-            Gecode::BoolVar theOne = Gecode::expr(*this, (tmp[0]) && (tmp[1]));
-            for(int j=2;j<i;j++){
-                theOne = Gecode::expr(*this, theOne && (tmp[j]));
-            }
-            cout << "conjunction " << std::endl;
-            return theOne;
 
-        } else if (neg) {
-            Gecode::BoolVar test = Gecode::expr(*this, not(parsePreconditions(neg->getGoal(), k, success)));
-            cout << "negative" << std::endl;
-            return test;
-        } else if (disj) {
-            cout << "disj" << std::endl;
-            const VAL::goal_list *goals;
-            goals = conj->getGoals();
-            Gecode::BoolVar *tmp;
-            int size = conj->getGoals()->size();
+            // create the expressions for the conjunction
+            Gecode::BoolVar result = Gecode::expr(*this, (temp[0]) && (temp[1]));
 
-            if (size < 2) {
-                *success = false;
-                return Gecode::BoolVar();
+            // if there are nested conjunctions (i > 0), create an expression for those aswell
+            for(int j = 2; j < i ; j++){
+                result = Gecode::expr(*this, result && (temp[j]));
             }
 
-            tmp = new Gecode::BoolVar[size];
+            return result;
+        }
+
+        // if the goal is a disjunction
+        if(disjunctionGoal) {
+            if(showOutput)
+                cout << *disjunctionGoal << "\n --- \n";
+
+            // get all goals of the disjunction
+            const VAL::goal_list *goals = disjunctionGoal->getGoals();
+
+            // number of subgoals
+            int goalCount = disjunctionGoal->getGoals()->size();
+
+            Gecode::BoolVar *temp = new Gecode::BoolVar[goalCount];
+
+
             int i = 0;
-            for (auto precon = goals->begin(); precon != goals->end(); precon++) {
-                tmp[i] = parsePreconditions(*precon, k, success);
+
+            // for each element of the disjunction, parse it again to see, if there are nested goals in it
+            for (list<VAL::goal*>::const_iterator precondition = goals->begin(); precondition != goals->end(); precondition++) {
+                temp[i] = parsePreconditions(*precondition, horizon);
                 i++;
             }
-            Gecode::BoolVar theOne = Gecode::expr(*this, (tmp[0]) || (tmp[1]));
-            for(int j=2;j<i;j++){
-                theOne = Gecode::expr(*this, theOne || (tmp[j]));
+
+            // create the expressions for the conjunction
+            Gecode::BoolVar result = Gecode::expr(*this, (temp[0]) || (temp[1]));
+
+            // if there are nested conjunctions (i > 0), create an expression for those aswell
+            for(int j = 2; j < i ; j++){
+                result = Gecode::expr(*this, result || (temp[j]));
             }
-            cout << "disjunction " << std::endl;
-            return theOne;
+
+            return result;
         }
     }
 
-
-    // our own solver
+    /**
+     * constructor for our solver
+     * @param domain the domain
+     * @param problem the problem
+     */
     Solver(VAL::domain *domain, VAL::problem *problem)
     {
         // --- Sources ---
@@ -131,38 +182,44 @@ public:
 
         int horizon = 10; // To construct a CSP from a planning problem, first choose a fixed horizon, which is the number of time steps over which to plan. [1]
 
+        // read predicates and actions from the domain and create the historys
         readDataFromDomain(horizon, domain);
 
-        // for each time point, connect preconditions with actions in same time window
-        for( int t = 0; t < horizon - 1; ++t ) {
-            for(auto a = domain->ops->begin();
-                a != domain->ops->end();
-                ++a)
-            {
-                //std::cout << "Preconditions for: " << (*a)->name->getName() << std::endl;
-                (*a)->precondition->display(0);
+        // connect precondition to action
+        for(int t = 0; t < horizon - 1; t++) {
 
-                // parse preconditions
+            // iterate over all actions
+            for(list<VAL::operator_*>::iterator action = domain->ops->begin(); action != domain->ops->end(); ++action) {
+
+                // boolean to check, if a precondition was found
                 bool success = true;
-                Gecode::BoolVar x = parsePreconditions((*a)->precondition, t, &success);
-                if (success) {
-                    Gecode::rel(*this, x);
-                } else {
 
-                    cout << "precondition empty" << std::endl;
-                }
-                // use result to define a gecode relation
+                // parse the preconditions
+                //cout << "action: " << *(*action)->name;
+                Gecode::BoolVar setPrecondition = parsePreconditions((*action)->precondition, t);
 
-                std::cout << std::endl;
+                // define the relationship, that the precondition has to be given
+                Gecode::rel(*this, setPrecondition);
             }
         }
+
+
+
+
+
+
+
+
+
+
+
 
 
         //effect constraints between state variables at time t, action variables at time t and state variables at time t + 1
         int x = 0;
         for( int t = 0; t < horizon - 1; ++t ) {
 
-            Gecode::rel(*this, sum(actionHistory_.at(t)) <= 1);
+            Gecode::rel(*this, sum(actionHistory.at(t)) <= 1);
 
             for(auto a = domain->ops->begin();
                 a != domain->ops->end();
@@ -171,15 +228,15 @@ public:
                 int aId = actionsMap.find((*a)->name->getName())->second;
                 for (auto i = (*a)->effects->add_effects.begin(); i != (*a)->effects->add_effects.end(); i++) {
                     int id = predicatesMap.find((*i)->prop->head->getName())->second;
-                    Gecode::rel(*this, predicatesHistory_.at(t + 1)[id] << actionHistory_.at(t)[aId]);
+                    Gecode::rel(*this, predicatesHistory.at(t + 1)[id] << actionHistory.at(t)[aId]);
                 }
 
                 for (auto i = (*a)->effects->del_effects.begin(); i != (*a)->effects->del_effects.end(); i++) {
                     int id = predicatesMap.find((*i)->prop->head->getName())->second;
-                    Gecode::rel(*this, !predicatesHistory_.at(t + 1)[id] << actionHistory_.at(t)[aId]);
+                    Gecode::rel(*this, !predicatesHistory.at(t + 1)[id] << actionHistory.at(t)[aId]);
                 }
 
-                std::cout << std::endl;
+                //std::cout << std::endl;
             }
         }
 
@@ -187,44 +244,27 @@ public:
         for(auto is = problem->initial_state->add_effects.begin(); is != problem->initial_state->add_effects.end(); ++is)
         {
             string effectName = (*is)->prop->head->getName();
-            std::cout << "EFFECT INITIAL:"  << (*is)->prop->head->getName() << std::endl;
+           // std::cout << "EFFECT INITIAL:"  << (*is)->prop->head->getName() << std::endl;
             //get initial effect id
             if ( predicatesMap.find(effectName) != predicatesMap.end() ) {
 
                 int effectId = predicatesMap.find(effectName)->second;
-                std::cout << "found at " << effectId;
+                std::cout << "found at " << effectId << "\n";
                 //constraint feature at time 0
-                Gecode::rel(*this, predicatesHistory_.at(0)[effectId] == 1);
+                Gecode::rel(*this, predicatesHistory.at(0)[effectId] == 1);
             }
         }
 
 
         //goal state constraints
-        bool success2 = true;
-        Gecode::rel(*this, parsePreconditions(problem->the_goal, horizon - 1, &success2));
+        Gecode::rel(*this, parsePreconditions(problem->the_goal, horizon - 1));
+
+
+        // taken from csp_test.cpp
 
         rg_ = Gecode::Rnd(time(NULL));
 
-        Gecode::branch(*this, actionHistory_.front(), Gecode::INT_VAR_RND(rg_),  Gecode::INT_VAL_RND(rg_));
-
-        options_.threads = 4;
-
-        search_ = new Gecode::DFS<Solver>(this, options_) ;
-
-        solution_ = NULL;
-
-
-
-
-
-        rg_ = Gecode::Rnd(time(NULL));
-
-        steps_ = Gecode::IntVarArray(*this, 4 , 0, 10);
-
-        Gecode::rel(*this, steps_[0]==steps_[1]);
-        Gecode::rel(*this, (steps_[1]!=steps_[2]) >> (steps_[3]==3));
-
-        Gecode::branch(*this, steps_, Gecode::INT_VAR_RND(rg_),  Gecode::INT_VAL_RND(rg_));
+        //Gecode::branch(*this, actionHistory_.front(), Gecode::INT_VAR_RND(rg_),  Gecode::INT_VAL_RND(rg_));
 
         options_.threads = 4;
 
@@ -233,6 +273,12 @@ public:
         solution_ = NULL;
 
     }
+
+
+
+
+
+
 
     // default constructor, taken from the csp_test.cpp
     Solver(bool share, Solver & s) : Gecode::IntMinimizeSpace(share,s)
@@ -273,7 +319,7 @@ public:
     Gecode::IntVarArray steps_;         // an IntVarArray with all taken steps
     Gecode::DFS<Solver> * search_;
     Solver * solution_;
-    Gecode::Rnd rg_;                    // a randome number
+    Gecode::Rnd rg_;                    // a random number
     Gecode::Search::Options options_;
 
 
@@ -295,7 +341,6 @@ int main(int argc, char ** argv)
     while(solver.solve())
     {
         solutionCnt ++;
-        //std::cout << "solution: " << solver.solution_->steps_[0].val() << " " << solver.solution_->steps_[1].val() << " " << solver.solution_->steps_[2].val() << " " << solver.solution_->steps_[3].val() << " " << std::endl;
     }
     std::cout << solutionCnt << " solutions found" <<  std::endl;
 
